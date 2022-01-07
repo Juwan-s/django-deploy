cbd62b25f8d3f0d7e98006640c6b9bff10cd53cd
Juwan-s <dlacksdn352@gmail.com>
2022-01-07 19:13:50 +0900

    allowed host update

--- webproj/settings.py
+++ webproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1hombxfz^(fg957om51%h$n%(u&aoatxiaca7z63v!est@-zfq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
